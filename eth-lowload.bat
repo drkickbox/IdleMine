d0b94227700b78b96e65320a1771fd8316b24498
drkickbox <89110389+drkickbox@users.noreply.github.com>
2022-03-28 14:53:33 -0400

    Add files via upload

---
+++ eth-lowload.bat
@@ -0,0 +1,5 @@
+if not DEFINED IS_MINIMIZED set IS_MINIMIZED=1 && start "" /min "%~dpnx0" %* && exit
+taskkill /F /im t-rex.exe
+"C:\Program Files (x86)\MSI Afterburner\MSIAfterburner.exe" -profile2
+"C:\Program Files\trexminer\t-rex.exe" --low-load 1 -a ethash -o stratum+tcp://us1.ethermine.org:4444 -u 0x742289060475750D1f76DbbC86C9646245031B33 -w desktop
+exit
---
+++ eth-stop.bat
@@ -0,0 +1,3 @@
+taskkill /F /im t-rex.exe
+taskkill /F /im t-rex-lowload.exe
+"C:\Program Files (x86)\MSI Afterburner\MSIAfterburner.exe" -profile1
